f9b5c8786970a8d04e0877d7225247d245f1f5c8
Thanos Polychronakis <thanpolas@gmail.com>
2023-03-20 14:12:27 +0200

    edit the deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,55 +1,61 @@
 // This is a script for deploying your contracts. You can adapt it to deploy
 // yours, or create new ones.
 
-const path = require("path");
+const path = require('path');
 
 async function main() {
   // This is just a convenience check
-  if (network.name === "hardhat") {
+  if (network.name === 'hardhat') {
     console.warn(
-      "You are trying to deploy a contract to the Hardhat Network, which" +
-        "gets automatically created and destroyed every time. Use the Hardhat" +
-        " option '--network localhost'"
+      'You are trying to deploy a contract to the Hardhat Network, which' +
+        'gets automatically created and destroyed every time. Use the Hardhat' +
+        " option '--network localhost'",
     );
   }
 
   // ethers is available in the global scope
   const [deployer] = await ethers.getSigners();
   console.log(
-    "Deploying the contracts with the account:",
-    await deployer.getAddress()
+    'Deploying the contracts with the account:',
+    await deployer.getAddress(),
   );
 
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+  console.log('Account balance:', (await deployer.getBalance()).toString());
 
-  const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
-  await token.deployed();
+  const Broadcasts = await ethers.getContractFactory('Broadcasts');
+  const broadcast = await Broadcasts.deploy();
+  await broadcast.deployed();
 
-  console.log("Token address:", token.address);
+  console.log('Broadcasts contract address:', broadcast.address);
 
   // We also save the contract's artifacts and address in the frontend directory
-  saveFrontendFiles(token);
+  saveFrontendFiles(broadcast);
 }
 
-function saveFrontendFiles(token) {
-  const fs = require("fs");
-  const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
+function saveFrontendFiles(contract) {
+  const fs = require('fs');
+  const contractsDir = path.join(
+    __dirname,
+    '..',
+    'frontend',
+    'src',
+    'contracts',
+  );
 
   if (!fs.existsSync(contractsDir)) {
     fs.mkdirSync(contractsDir);
   }
 
   fs.writeFileSync(
-    path.join(contractsDir, "contract-address.json"),
-    JSON.stringify({ Token: token.address }, undefined, 2)
+    path.join(contractsDir, 'contract-address.json'),
+    JSON.stringify({ Broadcasts: contract.address }, undefined, 2),
   );
 
-  const TokenArtifact = artifacts.readArtifactSync("Token");
+  const ContractArtifact = artifacts.readArtifactSync('Broadcasts');
 
   fs.writeFileSync(
-    path.join(contractsDir, "Token.json"),
-    JSON.stringify(TokenArtifact, null, 2)
+    path.join(contractsDir, 'Broadcasts.json'),
+    JSON.stringify(ContractArtifact, null, 2),
   );
 }
 
